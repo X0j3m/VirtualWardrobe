fafaa05feca1d9b556f20b4bdd8550b63564e5b5
Michał Ptasznik <michal.ptasznik@protonmail.com>
2025-07-30 15:12:45 +0200

    Initial commit

---
+++ src/main/java/x0j3m/virtualwardrobe/VirtualWardrobeApplication.java
@@ -0,0 +1,13 @@
+package x0j3m.virtualwardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualWardrobeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualWardrobeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/x0j3m/virtualwardrobe/VirtualWardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package x0j3m.virtualwardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualWardrobeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
