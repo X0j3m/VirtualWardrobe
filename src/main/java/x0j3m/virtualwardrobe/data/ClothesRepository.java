f136ccd3850f0a53f2470f0c3a558bf22233ed5a
Michał Ptasznik <michal.ptasznik@protonmail.com>
2025-08-10 17:34:51 +0200

    Added pagiation to ClothesRepository and ClothesService

--- src/main/java/x0j3m/virtualwardrobe/data/ClothesRepository.java
+++ src/main/java/x0j3m/virtualwardrobe/data/ClothesRepository.java
@@ -1,9 +1,10 @@
 package x0j3m.virtualwardrobe.data;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import x0j3m.virtualwardrobe.model.Clothes;
 
-public interface ClothesRepository extends CrudRepository<Clothes, Long> {
+public interface ClothesRepository extends CrudRepository<Clothes, Long>, PagingAndSortingRepository<Clothes, Long> {
     Iterable<Clothes> findByColor_Name(String colorName);
 
     Iterable<Clothes> findByType_Name(String typeName);
